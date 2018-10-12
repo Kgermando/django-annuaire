570e369ac9ba4f3dfc082c88290264b4e0f88d42
germain <germainkataku@hotmail.com>
2018-10-12 14:15:48 +0100

    Add heroku subdomain to ALLOWED_HOSTS

--- annuairerdc/settings.py
+++ annuairerdc/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY', '9afy0au25dd8antk7^b1(%69fxgt!nsrdzx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rdcannuaire.herokuapp.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'germain kataku <katakugermain@gmail.com>'
